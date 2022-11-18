f82f0a666b84508343699ca4ab838a6f8d791653
milianz <00053722@uca.edu.sv>
2022-11-17 20:59:04 -0600

    libreria

---
+++ BuscarEquipo.h
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+//Prototipos de las funciones.
+void BuscarEquipo();
+//Funcion para bucar un equipo en un fichero
+void BuscarEquipo()
+{
+    system("cls");
+
+    //Declaracion de la variable de tipo "archivo"
+    ifstream archivo;
+    archivo.open("RegistroEquipos.txt",  ios::in);
+
+    //Declaracion de variables normales.
+    int win, lose;
+    string nombre, nombreBuscar, connferencia;
+    bool equipoEncontrado = false;
+
+    //Limpiar el buffer.
+    fflush(stdin);
+
+    //Pedir datos al usuario.
+    cout << "\nDigite el nombre del equipo: ";
+    getline(cin, nombreBuscar);
+    archivo >> nombre;
+
+    cout<<"\n"<<"EQUIPO"<<"\t\t"<<"W"<<"\t"<<"L"<<"\t"<<"CONFERENCIA"<<endl;
+
+    //Buscando el equipo en el fichero...
+    while (!archivo.eof() && !equipoEncontrado)
+    {
+        archivo >> win;
+        archivo >> lose;
+        archivo >> connferencia;
+
+        if(nombre == nombreBuscar)
+        {
+            cout<<nombre<<"\t\t"<<win<<"\t"<<lose<<"\t"<<connferencia<<endl;
+            equipoEncontrado = true;
+        }
+
+        archivo >> nombre;
+    }
+
+    archivo.close();
+    
+    //Si el equipo no ha sido encontrado...
+    if(!equipoEncontrado)
+    {
+        cout << "El Equipo " << nombreBuscar << " no ha sido encontrado." << endl;
+    }
+
+}
--- main.cpp
+++ main.cpp
@@ -1,21 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include "BuscarEquipo.h"
+#include "insertarEquipo.h"
 using namespace std;
-//haciendo uso de listas;
-struct Nodo{
-    string equipo;
-    string conferencia;
-    int victorias, derrotas;
-    struct Nodo *sig;
-    struct Nodo *ant;
 
-};
-//inicilizanso la lista vacia.
-struct Nodo *lista=NULL;
-//empiezan las funciones.
-void insertarEquipoLista(string,string,int,int);
-void insertarEquipoFichero();
-//void imprimirLista();
 int main(){
 int opc;
     cout << "  .:MENU:." <<  endl;
@@ -28,64 +16,15 @@ int opc;
         switch (opc)
         {
         case 1:
-               insertarEquipoFichero();
+            insertarEquipoFichero();
             break;
 
         case 2:
-
+            BuscarEquipo();
             break;
         default:
             cout<<"Esa Opcion no existe";
             break;
         }
     return 0;
-}
-
-void insertarEquipoFichero(){
-
-    ofstream DBEquipos;
-    string  Equipo, Conferencia;
-    int Victorias, Derrotas;
-    DBEquipos.open("RegistroEquipos.txt",ios::out|ios::app);//creacion del archivo o abrir el archivo
-    
-    cout<<"Ingrese el nombre del equipo: ";
-    getline(cin,Equipo);
-    cout<<"Ingrese La conferencia del equipo: ";
-    getline(cin,Conferencia);
-    cout<<"Ingrese El Numero De Victorias: ";
-    cin>>Victorias;
-    cout<<"Ingrese La El Numero de Derrrotas: ";
-    cin>>Derrotas;
-    insertarEquipoLista(Equipo,Conferencia,Victorias,Derrotas);
-    DBEquipos<<Equipo<<"\t\t"<<Victorias<<"\t\t"<<Derrotas<<"\t\t"<<Conferencia<<"."<<endl;
-    DBEquipos.close();
-
-}
-
-
-void insertarEquipoLista(string Equipo,string Conferencia,int Victorias,int Derrotas){
-    struct Nodo *nuevoNodo = new Nodo();
-    struct Nodo *temp=lista;
-    if (nuevoNodo != NULL){
-        nuevoNodo->equipo = Equipo;
-        nuevoNodo->conferencia = Conferencia;
-        nuevoNodo->victorias =Victorias;
-        nuevoNodo->derrotas = Derrotas;
-        nuevoNodo->sig = NULL;
-        nuevoNodo->ant = NULL;
-        if (lista == NULL)
-        {
-            lista = nuevoNodo;
-        }
-        else
-        {
-            nuevoNodo->sig = lista;
-            lista->ant = nuevoNodo;
-            lista = nuevoNodo;
-        }
-    }
-    else
-    {
-        cout<<"No se puden agregar mas equipos.";
-    }
 }
